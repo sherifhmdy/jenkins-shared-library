8cc154bec690688d44b44c93dd2c2d98e1aabab3
Sherif Hamdy <sherifhmdy@icloud.com>
2023-06-03 17:28:26 +0300

    edit mavenAction method

--- vars/mavenAction.groovy
+++ vars/mavenAction.groovy
@@ -1,5 +1,8 @@
 def call(def input) {
     node {
-        sh "mvn $input.goal ${input.params ? input.params : ' '}"
+        sh '''
+            cd $WORKSPACE
+            mvn $input.goal ${input.params ? input.params : ' '}
+        '''
     }
 }
