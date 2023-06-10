93e1e2228a108cc09627211c8b5105bd718db87b
Sherif Hamdy <sherifhmdy@icloud.com>
2023-06-10 12:02:19 +0300

    small change

--- vars/sonarQube.groovy
+++ vars/sonarQube.groovy
@@ -4,13 +4,8 @@ def call() {
         //     sh 'mvn clean verify sonar:sonar -Dsonar.projectKey=helloworld -Dsonar.projectName=helloworld'
         // }
     // }
-        pipeline {
-            stages {
-                stage('hi') {
-                    steps {
-                        echo "hi"
-                    }
-                }
-            }
+        node {
+            stage 'hi'
+            echo "hello"
         }
 }
